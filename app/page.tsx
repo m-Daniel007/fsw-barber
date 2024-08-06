cf3b4d873572466b04de43089ef874c4ffd85b5d
Marcos <marcosdanieldossantos@yahoo.com.br>
2024-08-06 12:12:04 -0300

    feat: add header components and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Teste</Button>
-    </main>
+    <div>
+      <Header />
+
+      <div className="p-5">
+        <h2 className="text xl font-bold">Olá, Marcos!</h2>
+        <p>Terça-feira, 06 de Agosto</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Digite sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="h-[150px] relative mt-6 w-full">
+          <Image
+            src="/banner-01.png"
+            alt="Agende nas melhores barbearias"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
