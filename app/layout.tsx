c0696239e42ca15e1c4ea28499ddfde926934b3e
Marcos <marcosdanieldossantos@yahoo.com.br>
2024-08-09 20:24:01 -0300

    feat: user can serach for barbershops

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import "./globals.css"
+import { Toaster } from "sonner"
 import Footer from "./_components/footer"
 import AuthProvider from "./_providers/auth"
-import { Toaster } from "sonner"
+import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,10 +21,12 @@ export default function RootLayout({
     <html lang="pt" className="dark">
       <body className={inter.className}>
         <AuthProvider>
-          {children}
-          <Toaster />
-          <Footer />
+          <div className="flex h-full flex-col">
+            <div className="flex-1">{children}</div>
+            <Footer />
+          </div>
         </AuthProvider>
+        <Toaster />
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { SearchIcon } from "lucide-react"
 import Image from "next/image"
 import BarbershopItem from "./_components/barbershop-item"
 import BookingItem from "./_components/booking-item"
 import Header from "./_components/header"
+import Search from "./_components/search"
 import { Button } from "./_components/ui/button"
-import { Input } from "./_components/ui/input"
 import { quickSearchOptions } from "./_constants/search"
 import { db } from "./_lib/prisma"
 
@@ -27,10 +26,7 @@ export default async function Home() {
 
         {/* BUSCA*/}
         <div className="mt-6 flex items-center gap-2">
-          <Input placeholder="Digite sua busca..." />
-          <Button>
-            <SearchIcon />
-          </Button>
+          <Search />
         </div>
 
         {/* BUSCA RÁPIDA */}
